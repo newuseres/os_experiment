b83e42641f2ca25801931c5952d0ddc249da8e6e
newuseres <simper@139.com>
2023-12-21 11:06:33 +0800

    my work on lab2_1 is done.

--- kernel/vmm.c
+++ kernel/vmm.c
@@ -150,17 +150,17 @@ void kern_vm_init(void) {
 // application.
 //
 void *user_va_to_pa(pagetable_t page_dir, void *va) {
-  // TODO (lab2_1): implement user_va_to_pa to convert a given user virtual address "va"
-  // to its corresponding physical address, i.e., "pa". To do it, we need to walk
-  // through the page table, starting from its directory "page_dir", to locate the PTE
-  // that maps "va". If found, returns the "pa" by using:
-  // pa = PYHS_ADDR(PTE) + (va & (1<<PGSHIFT -1))
-  // Here, PYHS_ADDR() means retrieving the starting address (4KB aligned), and
-  // (va & (1<<PGSHIFT -1)) means computing the offset of "va" inside its page.
-  // Also, it is possible that "va" is not mapped at all. in such case, we can find
-  // invalid PTE, and should return NULL.
-  panic( "You have to implement user_va_to_pa (convert user va to pa) to print messages in lab2_1.\n" );
-
+//   TODO (lab2_1): implement user_va_to_pa to convert a given user virtual address "va"
+//   to its corresponding physical address, i.e., "pa". To do it, we need to walk
+//   through the page table, starting from its directory "page_dir", to locate the PTE
+//   that maps "va". If found, returns the "pa" by using:
+//   pa = PYHS_ADDR(PTE) + (va & (1<<PGSHIFT -1))
+//   Here, PYHS_ADDR() means retrieving the starting address (4KB aligned), and
+//   (va & (1<<PGSHIFT -1)) means computing the offset of "va" inside its page.
+//   Also, it is possible that "va" is not mapped at all. in such case, we can find
+//   invalid PTE, and should return NULL.
+  //panic( "You have to implement user_va_to_pa (convert user va to pa) to print messages in lab2_1.\n" );
+  return (void*)( lookup_pa(page_dir,(uint64)va) + ((uint64)va & ((1<<PGSHIFT) -1)) );
 }
 
 //
